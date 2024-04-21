8842277be972ab1eb6c6d3e5e31eecee77fdd661
Phygson <amalec9999@yandex.ru>
2024-04-21 22:48:35 +0300

    Hyprland workspaces

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -80,7 +80,23 @@
           "$mod, L, exec, gtklock -m /run/current-system/sw/lib/gtklock/powerbar-module.so -m /run/current-system/sw/lib/gtklock/playerctl-module.so -m /run/current-system/sw/lib/gtklock/userinfo-module.so"
           "SUPERALT, Q, exit"
           "$mod, D, exec, wofi -S drun"
-        ];
+        ] 
+        ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
       bindm = 
         [
           "$mod, mouse:272, movewindow"
