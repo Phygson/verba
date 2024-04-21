dd67f4fa3d711407b6bad8607ec1109afca147e8
Phygson <amalec9999@yandex.ru>
2024-04-21 15:38:56 +0300

    Mounts HDD

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -24,10 +24,17 @@
     ./hardware-configuration.nix
   ];
 
+
+  boot.supportedFilesystems = [ "ntfs" ];
   fileSystems = {
     "/".options = [ "compress=zstd" "noatime" ];
     "/home".options = [ "compress=zstd" "noatime" ];
     "/nix".options = [ "compress=zstd" "noatime" ];
+    "/data" = {
+      label = "Data";
+      fsType = "ntfs-3g";
+      options = [ "rw" ];
+    };
   };
 
 
