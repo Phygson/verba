9756c47f0f6bc8282a447bcc55b27e98aa7d3105
Phygson <amalec9999@yandex.ru>
2024-04-21 20:52:52 +0300

    Some hypr config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -82,6 +82,20 @@
           "$mod, mouse:272, movewindow"
           "$mod, mouse:273, resizewindow"
         ];
+      general = {
+        "gaps_in" = "2";
+        "gaps_out" = "5";
+        "border_size" = "2";
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+      };
+      decoration = {
+        "rounding" = "3";
+        "drop_shadow" = "yes";
+        "shadow_range" = "4";
+        "shadow_render_power" = "3";
+        "col.shadow" = "rgba(1a1a1aee)";
+      };
       input = {
         "kb_layout" = "us,ru";
         "kb_options" = "grp:alt_shift_toggle";
