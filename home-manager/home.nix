7be93344bfcfd965900f7d3e1104797989df295d
Phygson <amalec9999@yandex.ru>
2024-04-21 18:26:40 +0300

    Kitty

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -79,6 +79,13 @@
     ];
   };
 
+  programs.kitty = {
+    enable = true;
+    shellIntegration.enableFishIntegration = true;
+    settings.shell = "fish";
+    theme = "Gruvbox Dark";
+  };
+
   systemd.user.startServices = "sd-switch";
 
   home.stateVersion = "23.11";
