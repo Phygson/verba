f3f6eb78b6288b4a8f303b6e76dfdd1d30440bfa
Phygson <amalec9999@yandex.ru>
2024-04-25 23:42:01 +0300

    Moved hyprland config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,6 +9,7 @@
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
     outputs.homeManagerModules.rebuild
+    ./desktop/hyprland/hyprland.nix
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
@@ -57,6 +58,7 @@
                                qbittorrent
                                ruffle
                                libreoffice-fresh
+                               hunspellDicts.ru-ru
                                libqalculate
                                mpv
                              ];
@@ -78,85 +80,6 @@
   services.mpd = {
     enable = true;
     musicDirectory = /data/Music;
-  };
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    # enableNvidiaPatches = true;
-    extraConfig = ''
-      env = LIBVA_DRIVER_NAME,nvidia
-      env = XDG_SESSION_TYPE,wayland
-      env = GBM_BACKEND,nvidia-drm
-      env = __GLX_VENDOR_LIBRARY_NAME,nvidia
-      env = WLR_NO_HARDWARE_CURSORS,1
-    '';
-    settings = {
-      "$mod" = "SUPER";
-      exec-once = [
-        "swaync"
-      ];
-      bind = 
-        [
-          "$mod, B, exec, firefox"
-          "$mod, RETURN, exec, kitty"
-          "$mod, W, killactive"
-          "$mod, L, exec, gtklock -m ${pkgs.gtklock-powerbar-module.out}/lib/gtklock/powerbar-module.so -m ${pkgs.gtklock-playerctl-module.out}/lib/gtklock/playerctl-module.so -m ${pkgs.gtklock-userinfo-module.out}/lib/gtklock/userinfo-module.so"
-          "SUPERALT, Q, exit"
-          "$mod, D, exec, wofi -S drun"
-        ] 
-        ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-        builtins.concatLists (builtins.genList (
-            x: let
-              ws = let
-                c = (x + 1) / 10;
-              in
-                builtins.toString (x + 1 - (c * 10));
-            in [
-              "$mod, ${ws}, workspace, ${toString (x + 1)}"
-              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-            ]
-          )
-          10)
-      );
-      bindm = 
-        [
-          "$mod, mouse:272, movewindow"
-          "$mod, mouse:273, resizewindow"
-        ];
-      general = {
-        "gaps_in" = "2";
-        "gaps_out" = "5";
-        "border_size" = "2";
-        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-        "col.inactive_border" = "rgba(595959aa)";
-      };
-      decoration = {
-        "rounding" = "3";
-        "drop_shadow" = "yes";
-        "shadow_range" = "4";
-        "shadow_render_power" = "3";
-        "col.shadow" = "rgba(1a1a1aee)";
-      };
-      input = {
-        "kb_layout" = "us,ru";
-        "kb_options" = "grp:alt_shift_toggle";
-        "follow_mouse" = "1";
-      };
-      animations = {
-        "enabled" = "yes";
-        "bezier" = "myBezier, 0.05, 0.9, 0.1, 1.05";
-
-        "animation" = [ 
-          "windows, 1, 3, myBezier"
-          "windowsOut, 1, 3, default, popin 80%"
-          "border, 1, 4, default"
-          "fade, 1, 3, default"
-          "workspaces, 1, 2, default"
-        ];
-      };
-    };
   };
 
   programs.home-manager.enable = true;
