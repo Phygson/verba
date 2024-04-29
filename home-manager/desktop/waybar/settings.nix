a623085d6d228df21a59babc0595c34236132598
Phygson <amalec9999@yandex.ru>
2024-04-29 13:26:50 +0300

    Waybar update

--- home-manager/desktop/waybar/settings.nix
+++ home-manager/desktop/waybar/settings.nix
@@ -16,7 +16,7 @@
 
       modules-left = ["hyprland/workspaces"];
       modules-center = ["hyprland/window"];
-      modules-right = ["hyprland/language"];
+      modules-right = ["hyprland/language" "custom/notification"];
 
       "hyprland/workspaces" = {
         format = "{icon}";
@@ -48,6 +48,27 @@
           "(.*) - VSCodium" = "  $1  ";
         };
         separate-outputs = true;
+      };
+
+      "custom/notification" = {
+        tooltip = false;
+        format = "{icon}";
+        format-icons = {
+          notification = "<span foreground='red'><sup></sup></span>";
+          none = "";
+          dnd-notification = "<span foreground='red'><sup></sup></span>";
+          dnd-none = "";
+          inhibited-notification = "<span foreground='red'><sup></sup></span>";
+          inhibited-none = "";
+          dnd-inhibited-notification = "<span foreground='red'><sup></sup></span>";
+          dnd-inhibited-none = "";
+        };
+        return-type = "json";
+        exec-if = "which swaync-client";
+        exec = "swaync-client -swb";
+        on-click = "swaync-client -t -sw";
+        on-click-right = "swaync-client -d -sw";
+        escape = true;
       };
     };
   };
