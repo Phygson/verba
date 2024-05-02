f71b238a493f4a8da8ec11fda7da743a14eebd88
Phygson <amalec9999@yandex.ru>
2024-05-02 19:19:09 +0300

    ncmpcpp config

--- home-manager/desktop/music/ncmpcpp.nix
+++ home-manager/desktop/music/ncmpcpp.nix
@@ -1,15 +1,16 @@
 {pkgs, ...}: {
   programs.ncmpcpp = {
     enable = true;
-    package = pkgs.ncmpcpp.overrideAttrs (finalAttrs: previousAttrs: {
+    package = (pkgs.ncmpcpp.overrideAttrs (finalAttrs: previousAttrs: {
       patches = [./ncmpcppFixGenius.patch];
-    });
+    })).override {visualizerSupport=true;};
     settings = {
       lyrics_fetchers = "genius";
       follow_now_playing_lyrics = "yes";
       fetch_lyrics_for_current_song_in_background = "yes";
       media_library_primary_tag = "album_artist";
       startup_screen = "media_library";
+      screen_switcher_mode = "playlist, media_library, visualizer";
     };
   };
   home.shellAliases.ncmp = "ncmpcpp";
