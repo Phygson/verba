e07743d151aa1e68c8ffa32e58748d2581fa686a
Phygson <amalec9999@yandex.ru>
2024-05-02 18:02:11 +0300

    ncmpcpp library primary category is now album_artist

--- home-manager/desktop/music/ncmpcpp.nix
+++ home-manager/desktop/music/ncmpcpp.nix
@@ -8,6 +8,8 @@
       lyrics_fetchers = "genius";
       follow_now_playing_lyrics = "yes";
       fetch_lyrics_for_current_song_in_background = "yes";
+      media_library_primary_tag = "album_artist";
+      startup_screen = "media_library";
     };
   };
   home.shellAliases.ncmp = "ncmpcpp";
