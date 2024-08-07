b4e7e91275797ec646dbd10f52e9d05e8f840864
Phygson <amalec9999@yandex.ru>
2024-08-07 12:09:52 +0300

    fs

--- modules/nixos/system/fs.nix
+++ modules/nixos/system/fs.nix
@@ -1,18 +1,14 @@
 {
   boot.supportedFilesystems = ["btrfs" "ntfs"];
   fileSystems = {
-    "/".options = ["compress=zstd" "noatime"];
-    "/home".options = ["compress=zstd" "noatime"];
-    "/nix".options = ["compress=zstd" "noatime"];
+    "/".options = ["compress=zstd" "noatime" "space_cache=v2" "discard=async"];
+    "/home".options = ["compress=zstd" "noatime" "space_cache=v2" "discard=async"];
+    "/nix".options = ["compress=zstd" "noatime" "space_cache=v2" "discard=async"];
     "/data" = {
       device = "/dev/disk/by-uuid/D2E23D45E23D2EDB";
       fsType = "ntfs-3g";
       options = ["rw" "uid=1000" "nofail" "x-gvfs-show"];
     };
-    "/shared" = {
-      device = "/dev/disk/by-uuid/FA24F53724F4F789";
-      fsType = "ntfs-3g";
-      options = ["rw" "uid=1000" "nofail" "x-gvfs-show"];
-    };
+    "/SteamLibrary".options = ["compress=zstd" "noatime" "space_cache=v2" "discard=async" "nofail" "x-gvfs-show"];
   };
 }
